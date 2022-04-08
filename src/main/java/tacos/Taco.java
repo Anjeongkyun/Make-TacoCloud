2e6b65ef3e355d62c7ede7806f97ad35aa37d968
Anjeongkyun <dkswjdrbs12@naver.com>
2022-04-09 01:18:58 +0900

    1. Jdbc Template 적용 2. h2 database 환경설정

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,5 +1,6 @@
 package tacos;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
@@ -10,10 +11,14 @@ import lombok.Data;
 @Data
 public class Taco {
 	
+	private Long id;
+	
 	@NotNull
 	@Size(min = 5, message="최소 5개 이상의 문자가 되어야합니다.")
 	private String name;
 	
 	@Size(min=1, message = "최소 1개의 재료를 선택해야합니다.")
-	private List<String> ingredients;
+	private List<Ingredient> ingredients;
+	
+	private Date createdAt;
 }
--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,11 +1,12 @@
 package tacos.web;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -17,6 +18,8 @@ import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
+import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
 
 //자동으로 logger를 생성한다.
 //private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(DesignTacoController.class).; 와 같다.
@@ -25,20 +28,33 @@ import tacos.Taco;
 @RequestMapping("/design")
 public class DesignTacoController {
 	
+	private final IngredientRepository ingredientRepo;
+	private TacoRepository tacoRepo;
+	
+	@Autowired
+	public DesignTacoController(IngredientRepository ingredientRepo, 
+			TacoRepository tacoRepo) {
+		this.ingredientRepo = ingredientRepo;
+		this.tacoRepo = tacoRepo;
+	}
+	
 	@GetMapping
 	public String showDesignForm(Model model) {
-		List<Ingredient> ingredients = Arrays.asList(
-				new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-				new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-				new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-				new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-				new Ingredient("CHED", "Cheddar", Type.CHEESE),
-				new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-				new Ingredient("SLSA", "Salsa", Type.SAUCE),
-				new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
-				);
+//		List<Ingredient> ingredients = Arrays.asList(
+//				new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+//				new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
+//				new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
+//				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
+//				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
+//				new Ingredient("LETC", "Lettuce", Type.VEGGIES),
+//				new Ingredient("CHED", "Cheddar", Type.CHEESE),
+//				new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
+//				new Ingredient("SLSA", "Salsa", Type.SAUCE),
+//				new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
+//				);
+		
+		List<Ingredient> ingredients = new ArrayList<>();
+		ingredientRepo.findAll().forEach(i -> ingredients.add(i));
 		
 		Type[] types = Ingredient.Type.values();
 		for(Type type : types) {
