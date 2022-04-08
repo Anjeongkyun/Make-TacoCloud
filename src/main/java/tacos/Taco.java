0c0146b6f9d2cc6619ed4e2f4b472726d4d3c8e2
Anjeongkyun <dkswjdrbs12@naver.com>
2022-04-08 14:43:58 +0900

    1.  validation Dependency 추가 2. 도메인 클래스 유효성 조건 추가.

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -2,10 +2,18 @@ package tacos;
 
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class Taco {
+	
+	@NotNull
+	@Size(min = 5, message="최소 5개 이상의 문자가 되어야합니다.")
 	private String name;
+	
+	@Size(min=1, message = "최소 1개의 재료를 선택해야합니다.")
 	private List<String> ingredients;
 }
