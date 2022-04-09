5cfc40ae46b4a8f2a351ee87cd6c5218c92586f6
Anjeongkyun <dkswjdrbs12@naver.com>
2022-04-09 21:44:44 +0900

    JdbcTemplate을 이용한 insert 기능 개발완료

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -8,29 +8,42 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.Order;
+import tacos.data.OrderRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
 public class OrderController {
 	
+	private OrderRepository orderRepo;
+	
+	public OrderController(OrderRepository orderRepo) {
+		this.orderRepo = orderRepo;
+	}
+	
 	@GetMapping("/current")
-	public String orderForm(Model model) {
-		model.addAttribute("order",new Order());
-		
+	public String orderForm() {
 		return "orderForm";
 	}
 	
 	@PostMapping
-	public String processOrder(@Valid Order order, Errors errors) {
+	public String processOrder(@Valid Order order, Errors errors
+			,SessionStatus sessionStatus) {
 		if(errors.hasErrors()) {
 			return "orderForm";
 		}
 		
-		log.info("Order submitted : " + order);
+		orderRepo.save(order);
+		// session 재설정, 
+		// 만약 재설정하지 않으면 이전 주문 및 연관된 타코가 세션에 남아있게되어 
+		// 다음 주문에 포함되었던 타코 객체들을 가지고 시작할 수있다.
+		sessionStatus.setComplete();
 		return "redirect:/";
 	}
 }
