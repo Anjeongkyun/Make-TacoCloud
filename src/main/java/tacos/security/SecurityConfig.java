bf339319e7a82adaa1538f1ece8f921eec8fc1db
Anjeongkyun <dkswjdrbs12@naver.com>
2022-04-12 00:12:21 +0900

    1. Spring Security 진행중 (data, schema.sql 파일 작성) 2. 패스워드 bCrypt 해싱 암호화 진행

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package tacos.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation
 					.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation
@@ -8,10 +11,14 @@ import org.springframework.security.config.annotation.web
 					.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web
 					.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -25,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
+		/*auth.inMemoryAuthentication()
 		.withUser("user1")
 		.password("{noop}password1")
 		.authorities("ROLE_USER")
@@ -33,5 +40,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.withUser("user2")
 		.password("{noop}password2")
 		.authorities("ROLE_USER");
+		*/
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery(
+					"select username, password, enabled from users " +
+					" where username = ?")
+			.authoritiesByUsernameQuery(
+					"select username, authority from authorities " +
+					" where username = ?")
+			.passwordEncoder(new BCryptPasswordEncoder()); // BCryptoPasswordEncoder : bcrypt를 해싱 암호화한다.
 	}
 }
